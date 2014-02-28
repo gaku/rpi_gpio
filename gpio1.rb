e65001891bfddd86e87ed62b4dc3bf7823dd8ca5
Gaku Ueda <gaku@gaku.net>
2014-02-28 14:41:49 -0800

    Added comments.

--- gpio1.rb
+++ gpio1.rb
@@ -1,10 +1,12 @@
 require 'wiringpi'
 
-# WPI_MODE_PINS - WiringPi's own 'sane' pin numbering
+GPIO1 = 1 # GPIO 1 pin (Pin 12 on P1 header)
 
-GPIO1 = 1
+# Open GPIO interface.
+# By default, it uses WPI_MODE_PINS - WiringPi's own 'sane' pin numbering
+# For pin mapping, see: https://projects.drogon.net/raspberry-pi/wiringpi/pins/
 io = WiringPi::GPIO.new
-io.mode(GPIO1, OUTPUT)
+io.mode(GPIO1, OUTPUT)  # without this line, following write statements didnt' turn the signal on.
 io.write(GPIO1, 1)
 sleep(2)
 io.write(GPIO1, 0)
